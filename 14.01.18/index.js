43e4665de60bfa91ee1cb9eb1ecbcd2f2f08e034
Piotr Stefanowicz <stefan8357@gmail.com>
2018-01-14 22:23:11 +0100

    finding first missing integer

---
+++ 14.01.18/index.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 3, 7, 8];
+
+const findFirstMissingNumber = (array) => {
+  const lastNumberInSeries = array.reduce((firstNumber, nextNumber) => {
+    return nextNumber - firstNumber > 1 ? firstNumber : nextNumber
+  });
+  return lastNumberInSeries !== array[array.length - 1] ? lastNumberInSeries + 1 : 'Zbiór nie zawiera braków'
+};
+
+
+console.log(findFirstMissingNumber(arr));
