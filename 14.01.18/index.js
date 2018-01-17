d749b9cce7f93c3509d50e7f3469b922dbcd06f2
Piotr Stefanowicz <stefan8357@gmail.com>
2018-01-18 00:22:37 +0100

    adding absolute value at checking array length function

--- 14.01.18/index.js
+++ 14.01.18/index.js
@@ -1,5 +1,5 @@
 const findFirstMissingNumber = (array) => {
-  return array.length === 0 || array[0] - array[array.length - 1] < 2 ?
+  return array.length === 0 || Math.abs(array[0] - array[array.length - 1]) < 2 ?
     'Zbiór nie zawiera braków'
     :
     array.reduce((firstNumber, nextNumber) => {
@@ -7,4 +7,4 @@ const findFirstMissingNumber = (array) => {
     }) + 1
 };
 
-console.log(findFirstMissingNumber([0]));
+console.log(findFirstMissingNumber([-4, -3, -3, -2, 1, 2, 3, 7, 8]));
